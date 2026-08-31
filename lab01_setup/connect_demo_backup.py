95284b27666df2f892f7246a36ea028c005515d8
Hadis <diszszs707@gmail.com>
2026-08-31 15:01:30 +0700

    Remove temporary backup file

--- lab01_setup/connect_demo_backup.py
+++
@@ -1,7 +0,0 @@
-import socket
-import time
-
-s = socket.create_connection(("www.python.org", 443))
-print("Connected:", s.getpeername())
-time.sleep(5)  # keep it open so you can inspect it
-s.close()
